f189da1a0e72de6fd9e19688bd9203d27fb43250
Reginald Eli A. Deinla <aldrineg@localhost.localdomain>
2014-01-09 00:02:24 +0800

    wrong name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='django-oauth-tastypie',
+    name='django-oauth2-tastypie',
     version='0.1.0',
     package_dir={'': 'src'},
     py_modules=['authentication'],
